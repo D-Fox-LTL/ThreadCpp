eea784e239888d4a3c197628600c01f1bccc1746
D-Fox-LTL <99818044+D-Fox-LTL@users.noreply.github.com>
2024-01-03 15:57:08 +0100

    Add files via upload

---
+++ ThreadCppv2/ThreadCppv2/ThreadCppv2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "stdio.h"
+
+int RNDNumber;
+bool blClose = false;
+
+
+DWORD WINAPI RNDGenerator(LPVOID lpParam)
+{
+    for (;;) {
+        RNDNumber++;
+        if (RNDNumber > 6) RNDNumber = 1;
+        Sleep(200);
+    }
+
+
+    return 0;
+}
+
+DWORD WINAPI RNDReader(LPVOID lpParam)
+{
+    for (int i=0;i<20;i++) {
+        printf("%i, ", RNDNumber);
+        Sleep(200);
+    }
+
+    blClose = true;
+    return 0;
+}
+
+
+int main()
+{
+    DWORD ldwThread1Id, ldwThread2Id;
+    HANDLE lhThread2Id;
+
+
+
+    // init of variable
+    RNDNumber = 1;
+
+    //Start of RND thread
+    CreateThread(NULL, 0, RNDGenerator, NULL, 0, &ldwThread1Id);
+    lhThread2Id=CreateThread(NULL, 0, RNDReader, NULL, 0, &ldwThread2Id);
+    //RNDGenerator(NULL);
+
+    WaitForSingleObject(lhThread2Id, 20000);
+
+
+    std::cout << "Hello World!\n";
+}
